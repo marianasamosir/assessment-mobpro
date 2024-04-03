896492ed6e6e622368252f2b7f0a8f514ba0f9b7
Mariana <marmar.smsr@gmail.com>
2024-04-03 22:13:30 +0700

    Menambahkan Gambar di About

--- app/src/main/java/org/d3if3159/assessment1/ui/screen/AboutScreen.kt
+++ app/src/main/java/org/d3if3159/assessment1/ui/screen/AboutScreen.kt
@@ -3,6 +3,9 @@ package org.d3if3159.assessment1.ui.screen
 import android.content.res.Configuration
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -16,6 +19,8 @@ import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,12 +55,22 @@ fun AboutScreen(navController: NavHostController){
             )
         }
     ) {padding ->
-        Text(
-            text = stringResource(id = R.string.copyright),
-            modifier = Modifier
-                .padding(padding)
-                .padding(16.dp)
-        )
+
+        Column(
+            modifier = Modifier.padding(padding)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.burger),
+                contentDescription = "Ini sebuah gambar burger.",
+                modifier = Modifier
+                    .fillMaxWidth(),
+                contentScale = ContentScale.FillWidth
+            )
+            Text(
+                text = stringResource(id = R.string.copyright),
+                modifier = Modifier.padding(16.dp)
+            )
+        }
     }
 }
 
